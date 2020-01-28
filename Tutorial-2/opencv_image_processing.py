a2eebedcc6298fe5b6a245238e4d3b5a09b5085a
ujhoang <yoojehoang@gmail.com>
2020-01-28 17:23:16 +0000

    created a new images folder

--- Tutorial-2/opencv_image_processing.py
+++ Tutorial-2/opencv_image_processing.py
@@ -3,7 +3,7 @@ import cv2, imutils
 import time 
 import matplotlib.pyplot as plt
 
-name="uj.jpg"
+name="./images/uj.jpg"
 
 #loading the image, images are stored as (height, width, dimension)
 image = cv2.imread(name)
@@ -22,7 +22,7 @@ print("B={}, G={}, R={}".format(B, G, R))
 roi = image[80:270, 130:280]
 cv2.imshow("Region of Interest Dr", roi)
 cv2.waitKey(0)
-cv2.imwrite("cropped.jpg", roi)
+cv2.imwrite("./images/cropped.jpg", roi)
 '''
 #Resizing images
 #In deep learning we resize images, ignoring aspect ratios, 
@@ -32,7 +32,7 @@ cv2.imwrite("cropped.jpg", roi)
 resize = imutils.resize(image, width=300)
 cv2.imshow("resized uj", resize)
 cv2.waitKey(0)
-cv2.imwrite("resized.jpg", resize)
+cv2.imwrite("./images/resized.jpg", resize)
 
 
 ratio = 200/w
@@ -47,7 +47,7 @@ cv2.waitKey(0)
 rotated = imutils.rotate(image, 45)
 cv2.imshow("rotated uj", rotated)
 cv2.waitKey(0)
-cv2.imwrite("rotated.jpg", rotated)
+cv2.imwrite("./images/rotated.jpg", rotated)
 '''
 
 #Rotating image within boundary using imutils
@@ -55,7 +55,7 @@ cv2.imwrite("rotated.jpg", rotated)
 rotate_bound = imutils.rotate_bound(image, -45)
 cv2.imshow("rotated uj", rotate_bound)
 cv2.waitKey(0)
-cv2.imwrite("rotated in boundary.jpg", rotate_bound)
+cv2.imwrite("./images/rotated in boundary.jpg", rotate_bound)
 '''
 
 # Skeletonize the picture (edge detection)
@@ -64,7 +64,7 @@ grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 skeleton = imutils.skeletonize(grey, size=(3,3))
 cv2.imshow("matplot",skeleton)
 cv2.waitKey(0)
-cv2.imwrite("skeleton.jpg", skeleton)
+cv2.imwrite("./images/skeleton.jpg", skeleton)
 '''
 
 # Smoothing images
@@ -76,7 +76,7 @@ cv2.imwrite("skeleton.jpg", skeleton)
 blurred = cv2.GaussianBlur(image, (11,11), 10,0,10)
 cv2.imshow("blurred", blurred)
 cv2.waitKey(0)
-cv2.imwrite("blurred uj.jpg", blurred)
+cv2.imwrite("./images/blurred uj.jpg", blurred)
 '''
 
 # Drawing on an image
@@ -84,8 +84,23 @@ cv2.imwrite("blurred uj.jpg", blurred)
 # Hence, it is a good practice to create a copy (image.copy())
 # when drawing so we do not destroy the original image
 
+'''
 output = image.copy()
 cv2.rectangle(output, (140,90), (280,280),(0,0,255), 5)
 cv2.imshow("output rectangle", output)
 cv2.waitKey(0)
-cv2.imwrite("uj_rectangle.jpg", output)
+cv2.imwrite("./images/uj_rectangle.jpg", output)
+'''
+'''
+output = image.copy()
+cv2.circle(output, (140,280), 40, (50,255,50), -1) # -1 makes the circle solid fill
+cv2.imshow("circle", output)
+cv2.waitKey(0)
+cv2.imwrite("./images/uj_circle.jpg", output)
+'''
+
+output = image.copy()
+cv2.putText(output, "Yoo Je Hoang, Data Scientist", (10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.55, (255,255,255),1)
+cv2.imshow("uj with text", output)
+cv2.waitKey(0)
+cv2.imwrite("./images/uj_text.jpg", output)
