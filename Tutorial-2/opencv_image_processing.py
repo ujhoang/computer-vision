1be809c12ade1d079f384589ab1241f0c4ef7c91
ujhoang <yoojehoang@gmail.com>
2020-01-28 16:32:36 +0000

    update

--- Tutorial-2/opencv_image_processing.py
+++ Tutorial-2/opencv_image_processing.py
@@ -1,8 +1,9 @@
 # importing packages
 import cv2, imutils
 import time 
+import matplotlib.pyplot as plt
 
-name="ujsaved.jpg"
+name="uj.jpg"
 
 #loading the image, images are stored as (height, width, dimension)
 image = cv2.imread(name)
@@ -17,17 +18,51 @@ print("B={}, G={}, R={}".format(B, G, R))
 
 #Array slicing and cropping
 #extract region of interest starting from (320,60) to (420,160)
+'''
 roi = image[80:270, 130:280]
 cv2.imshow("Region of Interest Dr", roi)
 cv2.waitKey(0)
 cv2.imwrite("cropped.jpg", roi)
-
+'''
 #Resizing images
 #In deep learning we resize images, ignoring aspect ratios, 
 #so that the volume fits into a network which requires
 #the image to be square and to be of certain dimesion
-
-resize = cv2.resize(image, (200,200))
-cv2.imshow("resized photo", resize)
+'''
+resize = imutils.resize(image, width=300)
+cv2.imshow("resized uj", resize)
 cv2.waitKey(0)
 cv2.imwrite("resized.jpg", resize)
+
+
+ratio = 200/w
+dim = (200, int(h*ratio))
+resize = cv2.resize(image, dim)
+cv2.imshow("resized photo", resize)
+cv2.waitKey(0)
+'''
+
+# Rotating Images
+'''
+rotated = imutils.rotate(image, 45)
+cv2.imshow("rotated uj", rotated)
+cv2.waitKey(0)
+cv2.imwrite("rotated.jpg", rotated)
+'''
+
+#Rotating image within boundary using imutils
+'''
+rotate_bound = imutils.rotate_bound(image, -45)
+cv2.imshow("rotated uj", rotate_bound)
+cv2.waitKey(0)
+cv2.imwrite("rotated in boundary.jpg", rotate_bound)
+'''
+
+# Skeletonize the picture (edge detection)
+'''
+grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+skeleton = imutils.skeletonize(grey, size=(3,3))
+cv2.imshow("matplot",skeleton)
+cv2.waitKey(0)
+cv2.imwrite("skeleton.jpg", skeleton)
+'''
