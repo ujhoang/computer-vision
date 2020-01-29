4c32598adcde4d89fe398998794d0c79894d245c
ujhoang <yoojehoang@gmail.com>
2020-01-29 10:32:27 +0000

    Contour

--- Tutorial-2/object counter.py
+++ Tutorial-2/object counter.py
@@ -23,6 +23,7 @@ cv2.waitKey(0)
 grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("grey", grey)
 cv2.waitKey(0)
+'''
 cv2.imwrite("./images/grey tetris.jpg", grey)
 
 # 2. Edge Detection
@@ -30,5 +31,18 @@ edge = cv2.Canny(image, 30, 150)
 cv2.imshow("edged", edge)
 cv2.waitKey(0)
 cv2.imwrite("./images/edge tetris.jpg", edge)
-
+'''
 # 3. Thresholding
+# By thresholding the pixels <225 to 255 (White) - FOREGROUND
+# and pixels >=225 to 0 (Black), this segments the image - BACKGROUND
+# cv2.threshold[1] an array of pixel greyscale codes that would be used to create the edge detected image
+thresh = cv2.threshold(grey, 225, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
+cv2.imwrite("./images/black_white.jpg", thresh)
+
+# 4. Finding, Counting and Drawing Contour
+# Separating the background from the foreground is critical to image processing
+contours = cv2.findContours(thresh.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+cv2.imshow("Contours", contours)
+cv2.waitKey(0)
