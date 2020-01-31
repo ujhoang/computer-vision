cc089b7eefdb404a2003a573dc299f9e7ae4964c
ujhoang <yoojehoang@gmail.com>
2020-01-31 16:00:07 +0000

    update

--- 4. Optical Mark Recognition/Optical Mark Recognition.py
+++ 4. Optical Mark Recognition/Optical Mark Recognition.py
@@ -33,3 +33,51 @@ cv2.imshow("Original", image)
 cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Detect edges.
+# cnts return coordinates of all the edge points
+cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# docCnt returns four corner coordinates
+docCnt = None
+# check at least one contour was found
+if len(cnts) > 0:
+    # Sort the contours to size of area in descending order
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            docCnt = approx
+            break
+
+# Apply a top down view of the original image and the greyscale image
+paper = four_point_transform(image, docCnt.reshape(4,2))
+warped = four_point_transform(grey, docCnt.reshape(4,2))
+
+# To grade the documents, we need to do binarisation, a process of thresholding/segmenting
+# the foreground from the background image
+# Applying Otsu's thresholding method to binarise it
+
+thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+# Find contours in the thresh and draw on it
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+questionCnts = []
+
+for c in cnts:
+    # Compute the bounding box of the contour, then use the box to derive the aspect ratio
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w/float(h)
+
+    if w>= 20 and h >= 20 and ar >=0.9 and ar <=1.1:
+        questionCnts.append(c)
+
+
+cv2.drawContours(image, [questionCnts].reshape(), -1, (0, 255,0), 3)
+
+
+cv2.imshow("Threshold", image)
+cv2.waitKey(0)
