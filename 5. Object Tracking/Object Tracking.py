90676379b0397ee232bc1f78dbf8d2b9f194d6be
ujhoang <yoojehoang@gmail.com>
2020-03-22 18:01:35 +0000

    import files and construct argument parser

--- 5. Object Tracking/Object Tracking.py
+++ 5. Object Tracking/Object Tracking.py
@@ -1,2 +1,14 @@
 import cv2
+from collections import deque
+from imutils.Video import VideoStream
+import numpy as np 
+import argparse
+import imutils
+import time
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
 
